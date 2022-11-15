468f7cd0ada98855444af824494ff8c7014a5c9e
kimeu254 <jimkimeu254@gmail.com>
2022-11-15 08:28:04 +0300

    styling the default admin layout

--- resources/js/components/admin/layouts/Default.vue
+++ resources/js/components/admin/layouts/Default.vue
@@ -61,15 +61,12 @@
                     <router-view></router-view>
                 </main>
                 <footer class="footer">
-                    <div class="container-fluid d-flex justify-content-between">
-                        <span class="text-muted d-block text-center text-sm-start d-sm-inline-block">Copyright © bootstrapdash.com 2021</span>
-                        <span class="float-none float-sm-end mt-1 mt-sm-0 text-end"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin template</a> from Bootstrapdash.com</span>
+                    <div class="container-fluid">
+                        <span class="text-muted d-block text-center d-sm-inline-block">Copyright © 2022 All Rights Reserved</span>
                     </div>
                 </footer>
             </div>
-            
-        </div>
-        
+        </div>        
     </div>
 </template>
 
