a6e374968966819683a73d9a5cef9099119e4ecb
kimeu254 <jimkimeu254@gmail.com>
2022-11-22 21:16:07 +0300

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>South Eastern Brief - Wasya wa Ukambani</title>
+        <link rel="icon" href="{!! asset('img/journalism.png') !!}">
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
